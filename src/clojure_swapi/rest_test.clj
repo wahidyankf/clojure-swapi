b43b235b4bc88e80ba7afdc439d95771ff31d6b5
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-02-12 18:22:47 +0700

    change character to sw-char

--- src/clojure_swapi/rest_test.clj
+++ src/clojure_swapi/rest_test.clj
@@ -3,13 +3,13 @@
             [clojure.string :as string])
   (:require [clj-http.client :as client]))
 
-(defn get-character-movies [character-id]
-  (let [character (-> (str "https://swapi.dev/api/people/" character-id)
-                      client/get
-                      :body
-                      json/read-str)
+(defn get-sw-char-movies [sw-char-id]
+  (let [sw-char (-> (str "https://swapi.dev/api/people/" sw-char-id)
+                    client/get
+                    :body
+                    json/read-str)
         movies  (doall
-                 (->> (get character "films")
+                 (->> (get sw-char "films")
                       (map #(future (-> % client/get)))
                       (map deref)
                       (map (fn [res]
@@ -17,9 +17,9 @@
                                   :body
                                   json/read-str)))
                       (map #(get % "title"))))]
-    (str (get character "name") " was featured in: " (string/join ", " movies) ".")))
-(get-character-movies 1)
+    (str (get sw-char "name") " was featured in: " (string/join ", " movies) ".")))
+(get-sw-char-movies 1)
 ;; => "Luke Skywalker was featured in: A New Hope, The Empire Strikes Back, Return of the Jedi, Revenge of the Sith."
-(get-character-movies 20)
+(get-sw-char-movies 20)
 ;; => "Yoda was featured in: The Empire Strikes Back, Return of the Jedi, The Phantom Menace, Attack of the Clones, Revenge of the Sith."
 

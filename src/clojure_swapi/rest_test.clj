199696fc9253092bb2cc3af14094dda962e959b4
Wahidyan Kresna Fridayoka <wahidyankf@gmail.com>
2023-02-12 18:21:30 +0700

    change artist to character

--- src/clojure_swapi/rest_test.clj
+++ src/clojure_swapi/rest_test.clj
@@ -3,13 +3,13 @@
             [clojure.string :as string])
   (:require [clj-http.client :as client]))
 
-(defn get-artist-movies [artist-id]
-  (let [artist (-> (str "https://swapi.dev/api/people/" artist-id)
-                   client/get
-                   :body
-                   json/read-str)
+(defn get-character-movies [character-id]
+  (let [character (-> (str "https://swapi.dev/api/people/" character-id)
+                      client/get
+                      :body
+                      json/read-str)
         movies  (doall
-                 (->> (get artist "films")
+                 (->> (get character "films")
                       (map #(future (-> % client/get)))
                       (map deref)
                       (map (fn [res]
@@ -17,9 +17,9 @@
                                   :body
                                   json/read-str)))
                       (map #(get % "title"))))]
-    (str (get artist "name") " was featured in: " (string/join ", " movies) ".")))
-(get-artist-movies 1)
+    (str (get character "name") " was featured in: " (string/join ", " movies) ".")))
+(get-character-movies 1)
 ;; => "Luke Skywalker was featured in: A New Hope, The Empire Strikes Back, Return of the Jedi, Revenge of the Sith."
-(get-artist-movies 20)
+(get-character-movies 20)
 ;; => "Yoda was featured in: The Empire Strikes Back, Return of the Jedi, The Phantom Menace, Attack of the Clones, Revenge of the Sith."
 
